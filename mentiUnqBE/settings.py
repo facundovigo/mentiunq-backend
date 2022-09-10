cde6e10f5956012a3aa810480ef26ebfab61b47c
sirDemian <sirdemian@gmail.com>
2022-09-10 17:50:21 -0300

    fix ci github config

--- mentiUnqBE/settings.py
+++ mentiUnqBE/settings.py
@@ -87,11 +87,37 @@ WSGI_APPLICATION = 'mentiUnqBE.wsgi.application'
 #     }
 # }
 
+
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_DATABASE = os.environ.get("DB_DATABASE")
+DB_IS_AVAIL = all([
+        DB_USERNAME,
+        DB_PASSWORD,
+        DB_HOST,
+        DB_PORT,
+        DB_DATABASE
+])
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
+
+if DB_IS_AVAIL:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": DB_DATABASE,
+            "USER": DB_USERNAME,
+            "PASSWORD": DB_PASSWORD,
+            "HOST": DB_HOST,
+            "PORT": DB_PORT,
+        }
+    }
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
