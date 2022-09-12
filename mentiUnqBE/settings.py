546bf68130ad20a517d1197203c3b19f224217fb
sirDemian <sirdemian@gmail.com>
2022-09-12 09:51:22 -0300

    change setting github bbdd 2

--- mentiUnqBE/settings.py
+++ mentiUnqBE/settings.py
@@ -100,9 +100,9 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'mentiunq',
-           'USER': 'mentiunq',
-           'PASSWORD': 'mentiunq',
+           'NAME': 'postgres',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
            'HOST': '127.0.0.1',
            'PORT': '5432',
         }
