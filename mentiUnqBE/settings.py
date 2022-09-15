ea335b3afd1773c4194afbc8f28bb5b4f6dabdb7
facundo <facundovigo@gmail.com>
2022-09-15 15:34:20 -0300

    Deploy herooku parte 1

--- mentiUnqBE/settings.py
+++ mentiUnqBE/settings.py
@@ -169,9 +169,9 @@ CORS_ALLOW_HEADERS = (
 )
 
 
-CSRF_TRUSTED_ORIGINS = ['https://menti-backend.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://menti-backend.herokuapp.com', 'https://menti-unq.herokuapp.com/']
 
 CORS_ORIGIN_WHITELIST = (
-  'https://menti-backend.herokuapp.com',
+  'https://menti-unq.herokuapp.com/',
 )
 
