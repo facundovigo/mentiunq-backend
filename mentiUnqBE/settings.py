adc400fd563fbe2f8f0f2103cd7d7c5c64f09b61
sirDemian <sirdemian@gmail.com>
2022-09-15 17:04:41 -0300

    change ddbb local settings

--- mentiUnqBE/settings.py
+++ mentiUnqBE/settings.py
@@ -78,16 +78,17 @@ WSGI_APPLICATION = 'mentiUnqBE.wsgi.application'
 
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_DATABASE'),
-        'USER': config('DB_USERNAME'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+if config('ENVIRONMENT') == 'LOCALHOST':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_DATABASE'),
+            'USER': config('DB_USERNAME'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
     }
-}
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
