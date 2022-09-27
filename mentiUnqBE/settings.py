85d8f8ef9706cd3789d522282e79429c020b6d59
sirDemian <sirdemian@gmail.com>
2022-09-27 20:54:26 -0300

    CRUD User

--- mentiUnqBE/settings.py
+++ mentiUnqBE/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'mentiUnqBE.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_DATABASE'),
-        'USER': config('DB_USERNAME'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_DATABASE'),
+#         'USER': config('DB_USERNAME'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
